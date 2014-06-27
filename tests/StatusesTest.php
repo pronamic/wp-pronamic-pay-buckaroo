6c47bfef5a5f38c27de9b340420285e164bf2500
Remco Tolsma <info@remcotolsma.nl>
2014-06-27 16:50:48 +0200

    Added some inline documentation.

--- tests/StatusesTest.php
+++ tests/StatusesTest.php
@@ -21,18 +21,19 @@ class Pronamic_WP_Pay_Buckaroo_StatusesTest extends PHPUnit_Framework_TestCase {
 
 	public function statusMatrixProvider() {
 		return array(
+			// Success
 			array( Pronamic_WP_Pay_Buckaroo_Statuses::PAYMENT_SUCCESS, Pronamic_WP_Pay_Statuses::SUCCESS ),
-
+			// Failure
 			array( Pronamic_WP_Pay_Buckaroo_Statuses::PAYMENT_FAILURE, Pronamic_WP_Pay_Statuses::FAILURE ),
 			array( Pronamic_WP_Pay_Buckaroo_Statuses::VALIDATION_FAILURE, Pronamic_WP_Pay_Statuses::FAILURE ),
 			array( Pronamic_WP_Pay_Buckaroo_Statuses::TECHNICAL_ERROR, Pronamic_WP_Pay_Statuses::FAILURE ),
 			array( Pronamic_WP_Pay_Buckaroo_Statuses::PAYMENT_REJECTED, Pronamic_WP_Pay_Statuses::FAILURE ),
-
+			// Open
 			array( Pronamic_WP_Pay_Buckaroo_Statuses::WAITING_FOR_USER_INPUT, Pronamic_WP_Pay_Statuses::OPEN ),
 			array( Pronamic_WP_Pay_Buckaroo_Statuses::WAITING_FOR_PROCESSOR, Pronamic_WP_Pay_Statuses::OPEN ),
 			array( Pronamic_WP_Pay_Buckaroo_Statuses::WAITING_ON_CONSUMER_ACTION, Pronamic_WP_Pay_Statuses::OPEN ),
 			array( Pronamic_WP_Pay_Buckaroo_Statuses::PAYMENT_ON_HOLD, Pronamic_WP_Pay_Statuses::OPEN ),
-
+			// Cancelled
 			array( Pronamic_WP_Pay_Buckaroo_Statuses::CANCELLED_BY_CONSUMER, Pronamic_WP_Pay_Statuses::CANCELLED ),
 			array( Pronamic_WP_Pay_Buckaroo_Statuses::CANCELLED_BY_MERCHANT, Pronamic_WP_Pay_Statuses::CANCELLED ),
 		);
