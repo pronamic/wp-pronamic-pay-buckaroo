ec322f5748d78f3a7e88661da62faf6f22af1def
Remco Tolsma <info@remcotolsma.nl>
2014-11-18 11:33:18 +0100

    Added Buckaroo payment methods class.

---
+++ src/PaymentMethods.php
@@ -0,0 +1,109 @@
+<?php
+
+/**
+ * Title: Buckaroo payment methods constants
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2014
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
+class Pronamic_WP_Pay_Buckaroo_PaymentMethods {
+	/**
+	 * Indicator for the 'Achteraf Betalen' payment method
+	 *
+	 * @var string
+	 */
+	const PAYMENT_GUARANTEE = 'paymentguarantee';
+
+	/**
+	 * Indicator for the 'American Express' payment method
+	 *
+	 * @var string
+	 */
+	const AMERICAN_EXPRESS = 'amex';
+
+	/**
+	 * Indicator for the 'Bancontact / Mr Cash' payment method
+	 *
+	 * @var string
+	 */
+	const BANCONTACT_MISTER_CASH = 'bancontactmrcash';
+
+	/**
+	 * Indicator for the 'èM! Payment' payment method
+	 *
+	 * @var string
+	 */
+	const EM_PAYMENT = 'empayment';
+
+	/**
+	 * Indicator for the 'Giropay' payment method
+	 *
+	 * @var string
+	 */
+	const GIROPAY = 'giropay';
+
+	/**
+	 * Indicator for the 'iDEAL' payment method
+	 *
+	 * @var string
+	 */
+	const IDEAL = 'ideal';
+
+	/**
+	 * Indicator for the 'Maestro' payment method
+	 *
+	 * @var string
+	 */
+	const MAESTRO = 'maestro';
+
+	/**
+	 * Indicator for the 'MasterCard' payment method
+	 *
+	 * @var string
+	 */
+	const MASTERCARD = 'mastercard';
+
+	/**
+	 * Indicator for the 'Overschrijving' payment method
+	 *
+	 * @var string
+	 */
+	const TRANSFER = 'transfer';
+
+	/**
+	 * Indicator for the 'PayPal' payment method
+	 *
+	 * @var string
+	 */
+	const PAYPAL = 'paypal';
+
+	/**
+	 * Indicator for the 'paysafecard' payment method
+	 *
+	 * @var string
+	 */
+	const PAYSAFECARD = 'paysafecard';
+
+	/**
+	 * Indicator for the 'Sofortüberweisung' payment method
+	 *
+	 * @var string
+	 */
+	const SOFORTUEBERWEISING = 'sofortueberweisung';
+
+	/**
+	 * Indicator for the 'Ukash' payment method
+	 *
+	 * @var string
+	 */
+	const UKASH = 'Ukash';
+
+	/**
+	 * Indicator for the 'Visa' payment method
+	 *
+	 * @var string
+	 */
+	const VISA = 'visa';
+}
