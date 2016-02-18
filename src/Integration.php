229ade3551a3b5b7011c21ec1ff82ad55d3c9b71
Remco Tolsma <info@remcotolsma.nl>
2016-02-18 13:29:33 +0100

    Added get settings function.

--- src/Integration.php
+++ src/Integration.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * Title: Buckaroo integration
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2016
+ * Company: Pronamic
+ *
+ * @author Reüel van der Steege
+ * @version 1.2.2
+ * @since 1.0.0
+ */
 class Pronamic_WP_Pay_Gateways_Buckaroo_Integration extends Pronamic_WP_Pay_Gateways_AbstractIntegration {
 	public function __construct() {
 		$this->id       = 'buckaroo';
@@ -29,5 +39,20 @@ class Pronamic_WP_Pay_Gateways_Buckaroo_Integration extends Pronamic_WP_Pay_Gate
 
 	public function get_gateway_class() {
 		return 'Pronamic_WP_Pay_Gateways_Buckaroo_Gateway';
+	}
+
+	/**
+	 * Get required settings for this integration.
+	 *
+	 * @see https://github.com/wp-premium/gravityforms/blob/1.9.16/includes/fields/class-gf-field-multiselect.php#L21-L42
+	 * @since 1.2.2
+	 * @return array
+	 */
+	public function get_settings() {
+		$settings = parent::get_settings();
+
+		$settings[] = 'buckaroo';
+
+		return $settings;
 	}
 }
