0af0c60540a3775d0fed2273d0e28903e9ec1bc9
Remco Tolsma <info@remcotolsma.nl>
2016-02-24 15:13:36 +0100

    Fixed test.

--- tests/GatewayFactoryTest.php
+++ tests/GatewayFactoryTest.php
@@ -2,8 +2,6 @@
 
 class Pronamic_WP_Pay_Gateways_Buckaroo_GatewayFactoryTest extends WP_UnitTestCase {
 	function test_gateway_factory() {
-		Pronamic_WP_Pay_GatewayFactory::register( 'Pronamic_WP_Pay_Gateways_Buckaroo_Config', 'Pronamic_WP_Pay_Gateways_Buckaroo_Gateway' );
-
 		$config = new Pronamic_WP_Pay_Gateways_Buckaroo_Config();
 
 		$gateway = Pronamic_WP_Pay_GatewayFactory::create( $config );
