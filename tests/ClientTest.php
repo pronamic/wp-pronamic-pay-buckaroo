0b12409149a7e264405804ad78a09ca169ae8bab
Remco Tolsma <info@remcotolsma.nl>
2016-04-01 14:17:49 +0200

    Added a assert.

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -6,6 +6,8 @@ class Pronamic_WP_Pay_Gateways_Buckaroo_ClientTest extends WP_UnitTestCase {
 		$client->set_website_key( getenv( 'BUCKAROO_WEBSITE_KEY' ) );
 		$client->set_secret_key( getenv( 'BUCKAROO_SECRET_KEY' ) );
 
-		$client->get_issuers();
+		$issuers = $client->get_issuers();
+
+		$this->assertInternalType( 'array', $issuers );
 	}
 }
