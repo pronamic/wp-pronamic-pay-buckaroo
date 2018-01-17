07147affa29fa024165cf992d439df74e75d5008
Reüel van der Steege <reuel@pronamic.nl>
2018-01-17 11:56:53 +0100

    Happy 2018.

--- tests/SignatureTest.php
+++ tests/SignatureTest.php
@@ -3,7 +3,7 @@
 /**
  * Title: Buckaroo signature test.
  * Description:
- * Copyright: Copyright (c) 2005 - 2017
+ * Copyright: Copyright (c) 2005 - 2018
  * Company: Pronamic
  *
  * @see http://pronamic.nl/wp-content/uploads/2013/04/BPE-3.0-Gateway-HTML.1.02.pdf
