6d0d12a29387dece4ba1e474e125c399da074bc0
Remco Tolsma <info@remcotolsma.nl>
2018-03-01 20:06:44 +0100

    Fixed issue.

--- tests/ConfigProviderTest.php
+++ tests/ConfigProviderTest.php
@@ -2,6 +2,8 @@
 
 namespace Pronamic\WordPress\Pay\Gateways\Buckaroo;
 
+use Pronamic\WordPress\Pay\Core\ConfigProvider;
+
 class ConfigProviderTest extends \WP_UnitTestCase {
 	public function test_gateway_factory() {
 		ConfigProvider::register( 'buckaroo', __NAMESPACE__ . '\ConfigFactory' );
