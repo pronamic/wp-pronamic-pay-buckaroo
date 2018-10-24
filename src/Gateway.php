3d94b472448f70f0a46db780bceaf683d5e44814
Reüel van der Steege <reuel@pronamic.nl>
2018-10-24 11:53:37 +0200

    Rename payment `amount` to `total_amount`.

--- src/Gateway.php
+++ src/Gateway.php
@@ -136,7 +136,7 @@ class Gateway extends Core_Gateway {
 		$this->client->set_culture( $culture );
 		$this->client->set_currency( $payment->get_currency() );
 		$this->client->set_description( $payment->get_description() );
-		$this->client->set_amount( $payment->get_amount()->get_amount() );
+		$this->client->set_amount( $payment->get_total_amount()->get_amount() );
 		$this->client->set_invoice_number( Util::get_invoice_number( $this->client->get_invoice_number(), $payment ) );
 		$this->client->set_return_url( $payment->get_return_url() );
 		$this->client->set_return_cancel_url( $payment->get_return_url() );
